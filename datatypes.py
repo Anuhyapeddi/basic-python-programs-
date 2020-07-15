8d26886c4b50da11dd3ae8e5c465be1037fba614
Anuhyapeddi <42506400+Anuhyapeddi@users.noreply.github.com>
2020-07-15 17:16:32 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,36 @@
+#introduction to data_types in python 
+
+# integer
+a = 1 
+b = 4
+d = a+ b
+print(d)
+
+# decimal 
+a = 4.5
+print(a)
+
+#boolean (returns True or False value)
+a = 1
+print(bool(a))
+
+#string 
+a = "Happy to see you, this is Anuhya Shetty"
+print(a)
+
+#list(A single list can contain strings, integers, as well as objects, lists are mutable, represented in square bracket)
+list1 = [1, "a", "anuhya", 1+8]
+print(list1)
+
+#tuple(Tuple is also used to store data/values, tuples are immutable, represented in round bracket)
+tup = (1, "a", "chinnu")
+print(tup)
+
+#dictionary(stores both Key and Value, represented by curly brackets)
+dict = {1 : "apple", 2 : "banana", 3 : "candy", 4 : "dominos pizza"}
+print(dict)
+
+#set(The elements in the set are not repeated)
+s = set("Python is lub ")
+print(s)
+
