4b04710cf79fcaad9ca19f6511a765341327ec8a
Prathima Kadari <kadariprathima4@gmail.com>
2021-05-25 17:49:19 +0530

    Added factorial program

---
+++ factorial.py
@@ -0,0 +1,12 @@
+# Program to find factorial of a number
+
+def factorial(n):
+    i=1
+    fact = 1
+    while i < n+1:
+        fact = fact*i
+        i= i+1
+    print(fact)
+
+n = int(input('enter no to find factorial : '))
+factorial(n)
